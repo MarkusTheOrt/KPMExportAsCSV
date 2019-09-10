b36d431a76435999086ff98c33a16070a1d7af12
Markus Ort <markus@gameplayuniver.se>
2019-09-10 23:35:49 +0200

    Initial Commit

---
+++ index.js
@@ -0,0 +1,30 @@
+function RegexSearch(str) {
+  const regex = /(^Web\S+\sname:\s([\S\u0020]*)\n*^Web\S+\sURL:\s([\S\u0020]*)\n*^Login\sname:\s([\S\u0020]*)\n*^Login:\s([\S\u0020]*)\n*^Password:\s([\S\u0020]*)\n*^Comment:\s([\S\u0020]*)$)/gm;
+
+  let m;
+  var output = "url,username,password,extra\n";
+  while ((m = regex.exec(str)) !== null) {
+    // This is necessary to avoid infinite loops with zero-width matches
+    if (m.index === regex.lastIndex) {
+      regex.lastIndex++;
+    }
+
+    // The result can be accessed through the `m`-variable.
+
+    output += `${m[3]},${m[5]},${m[6]}, ${m[7]}\n`;
+  }
+  document.querySelector(".output").value = output;
+}
+
+// Sort for url,username,password,extra(comment)
+
+console.log(document.querySelector(".submit"));
+document.querySelector(".submit").addEventListener("click", () => {
+  var val = document.querySelector(".KPMFile").value;
+  RegexSearch(val);
+});
+
+document.querySelector(".copyOutput").addEventListener("click", () => {
+  document.querySelector(".output").select();
+  document.execCommand("copy");
+});
